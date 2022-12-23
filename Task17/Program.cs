9f0d1800e2ef0ef627e57afadd4d19765f931918
Artem <grekartemvl@mail.ru>
2022-12-23 20:20:22 +0500

    Add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,46 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты
+//точки (X и Y), X!=0 и Y!=0, и выдает номер четверти плоскости,
+// в которой находится эта точка
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int getQuarterNumber(int x, int y)
+{
+    int quarter = 0;
+    if (x > 0 && y > 0)
+    {
+        quarter = 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        quarter = 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+        quarter = 3;
+    }
+    else if (x > 0 && y < 0)
+    {
+        quarter = 4;
+    }
+    return quarter;
+}
+void showQuarter(int x, int y)
+{
+    if (x != 0 && y != 0)
+    {
+        int quarter = getQuarterNumber(x, y);
+        Console.WriteLine($"Точка [{x}, {y}] находится в {quarter} четверти");
+    }
+    else
+    {
+        Console.WriteLine($"Точка [{x}, {y}] находится на оси координат");
+    }
+}
+
+int userX = getUserValue("Введите X ");
+int userY = getUserValue("Введите Y ");
+showQuarter(userX, userY);
