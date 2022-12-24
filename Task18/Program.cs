bb49503db288343444aa89fee50f03a40c693d60
Artem <grekartemvl@mail.ru>
2022-12-24 14:21:18 +0500

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,44 @@
+﻿// 18. Напишите программу, которая по заданному номеру четверти
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+string getRangeFromQuarter(int quarter)
+{
+    string result = $"Диапазон значений в {quarter} четверти ";
+    switch (quarter)
+    {
+        case 1:
+        {
+            result += "x > 0 и y > 0";
+            break;
+        }
+        case 2:
+        {
+            result += "x < 0 и y > 0";
+            break;
+        }
+        case 3:
+        {
+            result += "x < 0 и y < 0";
+            break;
+        }
+        case 4:
+        {
+            result += "x > 0 и y < 0"; // result = result + "x > 0 и y < 0";
+            break;
+        }
+        default:
+        {
+            result = $"Четверти {quarter} не существует";
+            break;
+        }
+    }
+    return result;
+}
+int quarter = getUserValue("Введите номер четверти");
+string rangeDescription = getRangeFromQuarter(quarter);
+Console.WriteLine(rangeDescription);
